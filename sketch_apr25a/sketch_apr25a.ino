d2bd2241ad675b38dbd30e3a02137aea7d40f329
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2022-04-26 12:26:13 +0900

    수정

--- sketch_apr25a/sketch_apr25a.ino
+++ sketch_apr25a/sketch_apr25a.ino
@@ -2,13 +2,15 @@
 #define LED_Y 4       // Y의 핀 번호 4
 #define LED_G 5       // G의 핀 번호 5
 #define def_dis 11
+#define second 1000
+
 int r_time = 3;
 int y_time = 1;
 int g_time = 5;
 int distance = 0; //Cm로 계산된 거리값을 저장해두기위해 변수를 선언합니다.
 int count = 0;
 bool person = false;
-int time1 = 0;
+int light_time = 0;
 int time2 = 0;
 
 void setup() {
@@ -20,34 +22,44 @@ void setup() {
 }
  
 void loop() {
-  int second = 1000;
-
   measureDis();
-  if (time1 == 0){
-    turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_R, HIGH); 
-  }// 빨간 불 켜기
-  else if (time1 == r_time){
-    turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_G, HIGH);
-  }
-  else if (time1 == r_time + g_time){
-    turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_Y, HIGH);
-  }
-  else if (time1 == r_time + g_time + y_time){
-    time1 = -1;
-  }
+  light_time = control_time_light(light_time, r_time, y_time, g_time);
   if (time2 == 60) {
     time2 = -1;
     g_time = set_g_time(count);
     count = 0;
   }
-  time1 += 1;
+  light_time += 1;
   time2 += 1;
   Serial.println(time2);
   delay(second);
 }
+
+int control_time_light(int light_time, int r_time, int y_tim, int g_time){
+  if (light_time == 0){
+    turnOffAll();                  // 불 다 끄기
+    digitalWrite(LED_R, HIGH); 
+  }// 빨간 불 켜기
+  else if (light_time == r_time){
+    turnOffAll();                  // 불 다 끄기
+    digitalWrite(LED_G, HIGH);
+  }
+  else if (light_time == r_time + g_time){
+    turnOffAll();                  // 불 다 끄기
+    digitalWrite(LED_Y, HIGH);
+  }
+  else if (light_time == r_time + g_time + y_time){
+    light_time = -1;
+  }
+  return light_time;
+}
+
+void turnOffAll() {              // turnOffAll 함수 정의
+  digitalWrite(LED_R, LOW);      // 빨간 불 끄기
+  digitalWrite(LED_Y, LOW);      // 노란 불 끄기
+  digitalWrite(LED_G, LOW);      // 초록 불 끄기
+}
+
 int set_g_time(int count){
   int temp_g_time = 5 + count / 3;
   if (temp_g_time > 10){
@@ -57,6 +69,7 @@ int set_g_time(int count){
     return temp_g_time;
   }
 }
+
 void measureDis() {
     int volt = map(analogRead(A0), 0, 1023, 0, 5000);
 
@@ -66,11 +79,6 @@ void measureDis() {
     Serial.println(count);
 
     counter(distance);
-}
-void turnOffAll() {              // turnOffAll 함수 정의
-  digitalWrite(LED_R, LOW);      // 빨간 불 끄기
-  digitalWrite(LED_Y, LOW);      // 노란 불 끄기
-  digitalWrite(LED_G, LOW);      // 초록 불 끄기
 }
 
 void counter(int dis) {
