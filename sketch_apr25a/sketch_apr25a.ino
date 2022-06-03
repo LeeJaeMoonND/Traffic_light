5dd7ca21598fd37d133d75c74ee51574b3273d70
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2022-06-03 13:44:47 +0900

    증설

--- sketch_apr25a/sketch_apr25a.ino
+++ sketch_apr25a/sketch_apr25a.ino
@@ -1,6 +1,15 @@
-#define LED_R 3       // R의 핀 번호 3
-#define LED_Y 4       // Y의 핀 번호 4
-#define LED_G 5       // G의 핀 번호 5
+#define LED_UP_R 3       // R의 핀 번호 3
+#define LED_UP_Y 4       // Y의 핀 번호 4
+#define LED_UP_G 5       // G의 핀 번호 5
+
+#define LED_DOWN_R 6       // R의 핀 번호 6
+#define LED_DOWN_Y 7       // Y의 핀 번호 7
+#define LED_DOWN_G 8       // G의 핀 번호 8
+
+#define LED_CAR_R 9       // R의 핀 번호 6
+#define LED_CAR_Y 10       // Y의 핀 번호 7
+#define LED_CAR_G 11      // G의 핀 번호 8
+
 #define def_dis 9
 #define second 1000
 
@@ -16,16 +25,24 @@ int time2 = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_Y, OUTPUT);
-  pinMode(LED_G, OUTPUT);
+  pinMode(LED_UP_R, OUTPUT);
+  pinMode(LED_UP_Y, OUTPUT);
+  pinMode(LED_UP_G, OUTPUT);
+  pinMode(LED_DOWN_R, OUTPUT);
+  pinMode(LED_DOWN_Y, OUTPUT);
+  pinMode(LED_DOWN_G, OUTPUT);
+  pinMode(LED_CAR_R, OUTPUT);
+  pinMode(LED_CAR_Y, OUTPUT);
+  pinMode(LED_CAR_G, OUTPUT);
   Serial.begin(9600); 
 }
  
 void loop() {
   
-  measureDis();
-  //Serial.println(distance);
+  measureDis(1); // 밑 거리 측정
+  Serial.println(distance);
+
+
   //Serial.print("count ");
   //Serial.println(count);
   light_time = control_time_light(light_time, r_time, y_time, g_time);
@@ -47,15 +64,22 @@ void loop() {
 int control_time_light(int light_time, int r_time, int y_tim, int g_time){
   if (light_time == 0){
     turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_R, HIGH); 
+    digitalWrite(LED_UP_R, HIGH); 
+    digitalWrite(LED_DOWN_R, HIGH);
+    digitalWrite(LED_CAR_G, HIGH);      // 초록 불 끄기
   }// 빨간 불 켜기
   else if (light_time == r_time){
     turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_UP_G, HIGH);
+    digitalWrite(LED_DOWN_G, HIGH);
+    digitalWrite(LED_CAR_R, HIGH);      // 초록 불 끄기
+
   }
   else if (light_time == r_time + g_time){
     turnOffAll();                  // 불 다 끄기
-    digitalWrite(LED_Y, HIGH);
+    digitalWrite(LED_UP_Y, HIGH);
+    digitalWrite(LED_DOWN_Y, HIGH);
+    digitalWrite(LED_CAR_Y, HIGH);      // 초록 불 끄기
   }
   else if (light_time == r_time + g_time + y_time){
     light_time = -1;
@@ -64,9 +88,15 @@ int control_time_light(int light_time, int r_time, int y_tim, int g_time){
 }
 
 void turnOffAll() {              // turnOffAll 함수 정의
-  digitalWrite(LED_R, LOW);      // 빨간 불 끄기
-  digitalWrite(LED_Y, LOW);      // 노란 불 끄기
-  digitalWrite(LED_G, LOW);      // 초록 불 끄기
+  digitalWrite(LED_UP_R, LOW);      // 빨간 불 끄기
+  digitalWrite(LED_UP_Y, LOW);      // 노란 불 끄기
+  digitalWrite(LED_UP_G, LOW);      // 초록 불 끄기
+  digitalWrite(LED_DOWN_R, LOW);      // 빨간 불 끄기
+  digitalWrite(LED_DOWN_Y, LOW);      // 노란 불 끄기
+  digitalWrite(LED_DOWN_G, LOW);      // 초록 불 끄기
+  digitalWrite(LED_CAR_R, LOW);      // 빨간 불 끄기
+  digitalWrite(LED_CAR_Y, LOW);      // 노란 불 끄기
+  digitalWrite(LED_CAR_G, LOW);      // 초록 불 끄기
 }
 
 int set_g_time(int count){
@@ -79,12 +109,19 @@ int set_g_time(int count){
   }
 }
 
-void measureDis() {
-    int volt = map(analogRead(A0), 0, 1023, 0, 5000);
+void measureDis(int port) {
+    if (port = 0){
+      int volt = map(analogRead(A0), 0, 1023, 0, 5000);
+      distance = (27.61 / (volt - 0.1696)) * 1000; 
 
-    distance = (27.61 / (volt - 0.1696)) * 1000; 
+      counter(distance);
+    }
+    else if(port = 1){
+      int volt = map(analogRead(A1), 0, 1023, 0, 5000);
+      distance = (27.61 / (volt - 0.1696)) * 1000; 
 
-    counter(distance);
+      counter(distance);
+    }
     //Serial.println(distance);
 
 }
